cf13309e18e3f767c9c2c1faf51372203d30c987
Johnson Osei Yeboah <johnyoat@gmail.com>
2021-02-16 21:12:08 +0000

    Initial Commit

---
+++ Tenki/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Tenki
+//
+//  Created by Johnson Osei Yeboah on 16/02/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Tenki/TenkiApp.swift
@@ -0,0 +1,17 @@
+//
+//  TenkiApp.swift
+//  Tenki
+//
+//  Created by Johnson Osei Yeboah on 16/02/2021.
+//
+
+import SwiftUI
+
+@main
+struct TenkiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
