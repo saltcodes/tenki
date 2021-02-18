bf05fbd9917c51765ff9234d699b745cf5fe50de
Johnson Osei Yeboah <johnyoat@gmail.com>
2021-02-18 01:20:47 +0000

    added sfsymbol image and degrees text view

--- Tenki/ContentView.swift
+++ Tenki/ContentView.swift
@@ -9,8 +9,31 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ZStack{
+            LinearGradient(gradient: Gradient(colors: [Color.blue, Color.white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+            
+            VStack{
+                Text("Accra,GH")
+                    .font(.system(size: 32,weight:.medium,design:.default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing:8){
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    
+                    Text("32°")
+                        .font(.system(size: 70, weight: .medium))
+                        .foregroundColor(.white)
+                }
+                Spacer()
+            }
+        }
     }
 }
 
